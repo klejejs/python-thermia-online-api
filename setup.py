31524b0e49f7b65c3ae511f8ecf42a0a0f04698e
Krisjanis Lejejs <krisjanis.lejejs@gmail.com>
2021-12-25 21:02:56 +0200

    Added pypi support files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name='ThermiaOnlineAPI',
+  packages=['ThermiaOnlineAPI'],
+  version='1.0.0',
+  license='GPL-3.0',
+  description='A Python API for Thermia heat pumps using https://online.thermia.se',
+  author='Krisjanis Lejejs',
+  author_email='krisjanis.lejejs@gmail.com',
+  url='https://github.com/klejejs/python-thermia-online-api',
+  download_url='https://github.com/klejejs/python-thermia-online-api/archive/refs/tags/1.0.0.zip',
+  keywords=['Thermia', 'Online'],
+  install_requires=[],
+  classifiers=[],
+)
